3f28681822ae1faead248a61b40abdfdb3facb69
ABDELMOGHITH <143009449+abdelmoghith@users.noreply.github.com>
2023-11-23 23:18:14 +0100

    yes

---
+++ New folder/Calc.js
@@ -0,0 +1,18 @@
+let elemenet1 = document.getElementsByClassName("card");
+let elemenet2 = document.getElementsByTagName("p");
+let elemenet3 = document.querySelector(".allxi");
+let elemenet4 = document.querySelectorAll(".allxi");
+
+let elemenet5 = document.querySelector(".cardx");
+
+console.log(elemenet1);
+console.log(elemenet2);
+console.log(elemenet3);
+console.log(elemenet4);
+
+console.log(document.title);
+console.log(document.body);
+console.log(document.forms[0].one.value);
+
+console.log(elemenet5.innerHTML); // code html
+console.log(elemenet5.textContent); // content page html
